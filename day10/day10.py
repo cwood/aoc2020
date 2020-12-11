d413f75301553473f63835082ad7e9a003e3020f
Colin Wood <cwood06@gmail.com>
2020-12-10 23:51:59 -0800

    Le shrug local try

--- day10/day10.py
+++ day10/day10.py
@@ -26,10 +26,41 @@ def jolt_differences(jolts):
     return diffs
 
 
+def jolt_possibilities(jolts):
+
+    jolts.append(0)
+    jolts.sort()
+    jolts.insert(len(jolts), jolts[len(jolts)-1]+3)
+
+    pos = 1
+    i = 0
+    while i <= len(jolts)-1:
+        cjolt = jolts[i]
+
+        total_opts = 0
+        j = 0
+        while j <= len(jolts[0:i])-1:
+            jjolt = jolts[j]
+            for a in [1, 2, 3]:
+                if jjolt + a == cjolt:
+                    total_opts += 1
+            j += 1
+        i += 1
+
+        print(pos, total_opts)
+
+    print(pos)
+    return pos
+
+
 if __name__ == "__main__":
+    assert jolt_possibilities([16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]) == 8
+
     testjolts = [int(i) for i in open("testinput").readlines() if i]
     diff = jolt_differences(testjolts)
     assert diff[1] * diff[3] == 22 * 10  # 22 one jolts and 10 3 jolts
+    possible = jolt_possibilities(testjolts)
+    assert possible == 19208
 
     jolts = [int(i) for i in open("input").readlines() if i]
     realdiff = jolt_differences(jolts)
